0e4383887cb15ff54310267e0c5ca0d3d925fd4d
David Lewis <me@dlewis.info>
2019-05-26 21:07:56 -0400

    Updated confirmation message

--- mail.php
+++ mail.php
@@ -7,5 +7,6 @@ $recipient = "lewis.dm87@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Your message was sent to info@svbcfamily.com!";
+echo "If you do not get a reply within a couple of days please call the office at 540-868-4020 to ensure receipt."
 ?>
