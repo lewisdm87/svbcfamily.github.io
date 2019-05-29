61ee7e5c4dd1fdbce2f90cd58318101ea2e14ed9
David Lewis <me@dlewis.info>
2019-05-28 20:36:27 -0400

    Code correction

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "lewis.dm87@gmail.com";
+$recipient = "lewis.dm87@gmail.com, 5407714532@messaging.sprintpcs.com"; #personal email and text via email
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 
@@ -14,8 +14,4 @@ mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 # Notify user email was sent
 echo "Your message was sent to info@svbcfamily.com! \n";
 echo "If you do not get a reply within a couple of days please call the office at 540-868-4020 to ensure receipt.";
-
-# Notify recipient of new message from contact form
-mail("5407714532@txt.sprint.net", "", "You have a new form submission from svbcfamily.com!", "From: SVBCFAMILY.COM")
-
 ?>
