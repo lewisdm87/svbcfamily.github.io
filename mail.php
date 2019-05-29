cf2be5c9b875c5faa8b98766734946bf417eb81a
David Lewis <me@dlewis.info>
2019-05-28 20:27:07 -0400

    add text notification for new form response

--- mail.php
+++ mail.php
@@ -1,4 +1,5 @@
 <?php 
+# Gather Responses
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
@@ -6,7 +7,15 @@ $formcontent="From: $name \n Message: $message";
 $recipient = "lewis.dm87@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
+
+# Send an email to lewis.dm87@gmail.com
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+
+# Notify user email was sent
 echo "Your message was sent to info@svbcfamily.com! \n";
 echo "If you do not get a reply within a couple of days please call the office at 540-868-4020 to ensure receipt."
+
+# Notify recipient of new message from contact form
+mail("5407714532@txt.sprint.net", "", "You have a new form submission from svbcfamily.com!", "From: SVBCFAMILY.COM")
+
 ?>
