2c46688f0675033c5a48c3369d88c7b71b3cc25d
David M Lewis <me@dlewis.info>
2019-11-16 20:20:34 -0500

    Updated for 2019-11-17

--- private/slides.php
+++ private/slides.php
@@ -126,7 +126,7 @@ var x = setInterval(function() {
 <!--update weekly slides    w=950, h=520-->
 <div class="slides">
 
-<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQLnWUnZL0ZHs2hQqMPQvcPah0LouO9950Wgp8EzNHiArISS4qRStFI25OAzsGaGm-vq4bT4lPKu28H/embed?start=false&loop=true&delayms=10000" frameborder="0" width="900" height="515" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQyHHs7DlAP2Fi3o5T28z_kml0lyhpZ_FJa440yZnPbs6NcQPvPbJz-T46psJAB3gx5K9PwF70_99xO/embed?start=false&loop=true&delayms=10000" frameborder="0" width="900" height="515" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
 
 </div>
 
