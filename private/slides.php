d0fbe423521f129aeb4558c111f22eca2baff23f
David M Lewis <me@dlewis.info>
2019-12-28 16:39:35 -0500

    Updated for 2019-12-29

--- private/slides.php
+++ private/slides.php
@@ -125,9 +125,9 @@ var x = setInterval(function() {
 
 <!--update weekly slides    w=950, h=520-->
 <div class="slides">
-	
-<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSwH6DCFhuUA-3g__75RZNzDBLv4EVHX_Uyhm_IoYrEL4Lanz4Ej820X4hXeTK523YjvmApKJFpkNQX/embed?start=false&loop=true&delayms=10000" frameborder="0" width="950" height="520" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
 
+<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSPKG068qWa-OroZiEXLA3FBRNeGz8ffRo4sww8jB6UOF3xQ0lAAMdj7XbwomgfocHmPaOzUCehBbrZ/embed?start=false&loop=true&delayms=10000" frameborder="0" width="950" height="520" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+	
 </div>
 
 <div class="temps">
