6bd0fcbf2240b78a229ca6a9f2726237af409e0f
David M Lewis <me@dlewis.info>
2020-03-07 16:46:45 -0500

    Updated pages to allow shared code

---
+++ includes/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/svbcfamily.css"
+	
+
+<body>
+<header>
+		<a href="#main-menu" class="menu-toggle">
+			<span class="fa fa-list"></span>
+		</a>
+		<img src="media/logo_200x200.png" width="50" height="50" alt="svbc logo"/>
+	<nav id="main-menu" class="main-menu">
+    	<a href="#main-menu-toggle" class="menu-close">
+		<span class="fa fa-window-close"></span>
+    	</a>
+		<ul>
+		<li><a href="index.php">HOME</a></li>
+		<li><a href="about.php">ABOUT</a></li>
+		<li><a href="awana.php">AWANAS</a></li>
+		<li><a href="calendar.php">CALENDAR</a></li>
+		<li><a href="contact.php">CONTACT</a></li>
+		<li><a href="events.php">EVENTS</a></li>
+		<li><a href="photos.php">PHOTOS</a></li>
+		<li></li>
+		<li></li>
+		<li><img src="media/logo_200x200.png" width="50" height="50" alt="svbc logo"/></li>
+    	</ul>
+  	</nav>
+  
+  	<a href="#main-menu-toggle"
+     class="backdrop"
+     tabindex="-1"></a> 
+     
+</header>
