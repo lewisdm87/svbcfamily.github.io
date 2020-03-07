0765c8d4e983ee5a217745aecfb1789128e4856b
David M Lewis <me@dlewis.info>
2020-03-07 16:46:18 -0500

    Updated pages to allow shared code

---
+++ awana.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>AWANA</title>
+	
+<meta name="Description" content="A page devoted to our Awana Clubs. We believe that raising children to follow Christ leaves a better future for us all.">
+
+<meta name="robots" content="index, follow">
+	
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	
+<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
+<link rel="stylesheet" href="../css/svbcfamily.css"/>
+
+<!-- Add Icons for hamburger menu and page close icon -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="page">
+	
+
+<header>
+<?php
+	include('includes/header.php');
+?>
+</header>
+
+	
+	<section class="title"><h1>Awana Clubs</h1></section>
+	
+	
+	<div class="awana">
+		<section class="mission">
+			<h1>Our Mission</h1>
+			<p>Awana's goal at Shenandoah Valley Baptist Church is to reach boys and girls with the gospel of Christ and train them to serve Him. We have four groups in our club... Cubbies, Spars, Truth & Training, and Trek.</p>	
+			<p>In Awana we have quarterly themes, exciting songs, Bible stories, interactive games, and small group time learning verses.</p>
+		</section>
+	
+			
+		<section class="meetings"><h1>Meetings</h1>
+			<p>Awana Clubs meets ever Wednesday night September - May from 6:30pm - 8:00pm at the Chruch.</p>
+		
+			<p>We meet in the gym and end our night downstairs in the main building.</p>
+		</section>
+		
+		<section class="ages"><h1>Age Ranges</h1>
+			<table class="table-ages">
+			<tr>
+				<td>Cubbies</td>
+				<td>:</td>
+				<td>Pre-School</td>
+			</tr>
+			<tr>
+				<td>Sparks</td>
+				<td>:</td>
+				<td>K5 - 2nd Grade</td>
+			</tr>
+			<tr>
+				<td>Truth & Training</td>
+				<td>:</td>
+				<td>3rd - 5th Grade</td>
+			</tr>
+			<tr>
+				<td>Trek</td>
+				<td>:</td>
+				<td>6th - 8th Grade</td>
+			</tr>
+			</table>
+		</section>
+
+	
+	</div>
+	
+	
+<!-- <p><a href="docs/awana-calendar.pdf" class="nav">2018-2019 Awana Calendar</a></p> --> <!-- calendar place holder -->
+	
+	<p>Be sure to follow our Facebook Page for updates and photos @ <a href="https://wwww.facebook.com/svbc.awana" class="nav" title="Awanas on Facebook">Awana Facebook!</a></p>
+
+<footer>
+	<?php
+		include('includes/footer.php');
+	?>	
+</footer>
+	
+	</div>
+</body>
+</html>
