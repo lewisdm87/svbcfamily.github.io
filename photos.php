b614e1d149b26ea0f845bdb23d08008c0fd38d81
David M Lewis <me@dlewis.info>
2020-03-07 16:46:57 -0500

    Updated pages to allow shared code

---
+++ photos.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<title>HOME</title>
+	
+	<meta name="robots" content="index, follow">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>	
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	
+<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
+<link rel="stylesheet" href="../css/svbcfamily.css"/>
+
+<!-- Add Icons for hamburger menu and page close icon -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="page">
+	
+
+<header>
+<?php
+	include('includes/header.php');
+?>
+</header>
+	
+<div class="content-slides">
+
+<div class="container">
+  <h2>Quick Highlights</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="media/web-slides/svbc1.jpg" alt="baptism01" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="media/web-slides/svbc2.jpg" alt="baptism02" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="media/web-slides/svbc3.jpg" alt="fellowship01" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/web-slides/svbc4.jpg" alt="greeting" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/web-slides/svbc5.jpg" alt="trammel-quartet" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/web-slides/svbc6.jpg" alt="fellowship02" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/web-slides/svbc7.jpg" alt="movie-on-lawn" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/web-slides/svbc8.jpg" alt="kidzone" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/web-slides/svbc9.jpg" alt="awana" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/web-slides/svbc10.jpg" alt="musselman-concert" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/web-slides/svbc11.jpg" alt="fellowship03" style="width:100%;">
+      </div>
+		
+		<div class="item">
+        <img src="media/web-slides/svbc12.jpg" alt="main-entrance" style="width:100%;">
+      </div>
+		
+		<div class="item">
+        <img src="media/web-slides/svbc13.jpg" alt="vbs01" style="width:100%;">
+      </div>
+		
+		<div class="item">
+        <img src="media/web-slides/svbc14.jpg" alt="vbs02" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+<p>
+	<a href="https://www.facebook.com/svbcfamily/photos" class="nav">More</a> photos avilable on our FaceBook page!</p>
+</div>
+	
+	
+<footer>
+	<?php
+		include('includes/footer.php');
+	?>	
+</footer>
+
+</div>
+</body>
+</html>
