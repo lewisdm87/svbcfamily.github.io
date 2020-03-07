6b9408b37e06e9837ea9dcf6a3169481ec65588d
David M Lewis <me@dlewis.info>
2020-03-07 16:46:39 -0500

    Updated pages to allow shared code

---
+++ includes/footer.php
@@ -0,0 +1,38 @@
+	<div class="final">
+		<h2><span style="text-decoration: overline">CONTACT INFO</span></h2>
+		<br>
+		<p>4699 Valley Pike</p>
+		<p>Stephens City, VA 22655</p>
+		<br>
+		<p>Office: 540-868-4020</p>
+		<p>Fax: 540-869-4679</p>
+		<br>
+		<a href="mailto:info@svbcfamily.com" title="Send Us an E-Mail">info@svbcfamily.com</a>
+	</div>
+	
+	<div class="footer-copyRight">
+		Copyright &copy; 2019 Shenandoah Valley Baptist Church. All rights reserved.
+	</div>
+		
+ 	<div class="footer-social">
+		<a href="https://www.facebook.com/svbcfamily" 
+		  title="facebook.com/svbcfamily"><img src="media/ico/fb.png" width="25" height="25" alt="fb-svbc"/>		    </a>
+			
+		<a href="https://www.instagram.com/svbcfamily" 
+		  title="@svbcfamily"><img src="media/ico/insta.png" width="25" height="25" alt="instagram"/>
+		</a>			
+			
+		<a href="https://www.twitter.com/svbcfamily" 
+		  title="@svbcfamily"><img src="media/ico/twitter.png" width="25" height="25" alt="twitter"/>
+		</a>	
+			
+		<a href="https://www.youtube.com/channel/UCVNMH7wzOzSwZ6l-HnoKC7g" 
+		  title="SVBC on YouTube"><img src="media/ico/youTube.png" width="25" height="25" alt="YouTube"/>
+		</a>
+			
+		<a href="https://www.facebook.com/svbc.awana" 
+		  title="facebook.com/svbc.awana"><img src="media/ico/fb.png" width="25" height="25" alt="fb-awana"/>
+		</a>
+	</div>
+		
+		
