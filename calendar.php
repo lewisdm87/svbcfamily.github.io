4aa19d912ce6e97a5271072d08452166b915d98d
David M Lewis <me@dlewis.info>
2020-03-07 16:46:15 -0500

    Updated pages to allow shared code

---
+++ calendar.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<title>CALENDAR</title>
+	
+<meta name="Description" content="Want to see what we're all about before joining us for services? That's okay, most of our events are open to the public and we welcome anyone to join us for our highlight events.">
+
+<meta name="robots" content="index, follow">
+	
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	
+<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
+<link rel="stylesheet" href="../css/svbcfamily.css"/>
+
+<!-- Add Icons for hamburger menu and page close icon -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="page">
+	
+
+<header>
+<?php
+	include('includes/header.php');
+?>
+</header>
+
+
+
+
+<div class="g-cal">
+	<h2>Scheduled Services</h2>
+<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=YWhtdWxwOGpiamlhZ3RzNXZnMXBpc2Vpa2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23009688&amp;color=%230B8043&amp;mode=AGENDA" style="border-width:0" width="960" height="600"></iframe>
+</div>
+
+<section class="final">	
+	  <div class="bottom-contact">
+		<h2><span style="text-decoration: overline">CONTACT INFO</span></h2>
+		<br>
+		<p>4699 Valley Pike</p>
+		<p>Stephens City, VA 22655</p>
+		<br>
+		<p>Office: 540-868-4020</p>
+		<p>Fax: 540-869-4679</p>
+		<br>
+		<a href="mailto:info@svbcfamily.com" title="Send Us an E-Mail">info@svbcfamily.com</a>
+	</div>
+		
+</section>
+
+<footer>
+	<?php
+		include('includes/footer.php');
+	?></footer>
+	
+	</div>
+
+</body>
+</html>
