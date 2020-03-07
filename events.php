c199bcf7a26f8914236887297947da2ab0c328e4
David M Lewis <me@dlewis.info>
2020-03-07 16:46:36 -0500

    Updated pages to allow shared code

---
+++ events.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>EVENTS</title>
+	
+<meta name="Description" content="We believe that church should be about more than listening to a sermon on Sunday. To help grow in Christ come out and be involved! There's something for everyone here at SVBC.">
+
+<meta name="robots" content="index, follow">
+	
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	
+<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
+<link rel="stylesheet" href="../css/svbcfamily.css"/>
+
+<!-- Add Icons for hamburger menu and page close icon -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="page">
+	
+
+<header>
+<?php
+	include('includes/header.php');
+?>
+</header>
+
+<?php
+	include('events/hfava.html');
+?>
+
+<vr>
+
+<?php
+	include('events/movie-night.html');
+?>
+
+<vr>
+
+<?php
+	include('events/bikes.html');
+?>
+
+<footer>
+<?php
+	include('includes/footer.php');
+?>
+</footer>
+
+</div>
+
+</body>
+</html>
