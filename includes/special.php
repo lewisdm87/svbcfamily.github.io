37d229c962d2ac0a6a7a2a4b478f4727adfc6fe7
David M Lewis <me@dlewis.info>
2020-03-14 20:37:00 -0400

    Canceled services mar 15, 2020

---
+++ includes/special.php
@@ -0,0 +1,3 @@
+ <div class="special">
+	<p> For health and safety concerns services are canceled for Sunday Mar 15, 2020. Services scheduled to resume Wednesday Mar Mar 18, 2020 at 6:30pm. </p>
+</div>
