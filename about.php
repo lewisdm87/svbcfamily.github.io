dca5a6bf24fdb43e828cc9bf637b06e88ec39be4
David M Lewis <me@dlewis.info>
2020-03-15 20:59:55 -0400

    Formatted readability

--- about.php
+++ about.php
@@ -1,110 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html> 
+<html lang="en"> 
+
 <head>
-
-<title>ABOUT</title>
-
-<meta name="Description" content="A bit more detail about SVBC that didn't quite make the home page. Includes our statement of faith, the 5 G's, our purpose, and some information on the Great Commandment/Comission as described in the word of God">
-
-<meta name="robots" content="index, follow">
-	
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	
-<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
-<link rel="stylesheet" href="../css/svbcfamily.css"/>
-
-<!-- Add Icons for hamburger menu and page close icon -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
+	<title>ABOUT</title>
+	<meta name="Description" content="A bit more detail about SVBC that didn't quite make the home page. Includes our statement of faith, the 5 G's, our purpose, and some information on the Great Commandment/Comission as described in the word of God">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="widthÞvice-width, initial-scale=1.0"/>
+	<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/> <link rel="stylesheet" href="../css/svbcfamily.css"/>
+	<!-- Add Icons for hamburger menu and page close icon --> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
-<body>
-<div class="page">
-	
-<header>
-<?php
-	include('includes/header.php');
-?>
-</header>
 
+<body> <div class="page">
+	<header> <?php include('includes/header.php'); ?> </header>
 	
 	<section class="title"><h1>About our Church</h1></section>
 	
-	<div class="about">
-	<section class="faith">
-	<h2>Statement of Faith</h2>
-<ol>
-<li>We believe in the verbal inspiration and authority of the Scriptures. We believe that the Bible reveals God, the fall of man, the way of salvation, and God's plan and purpose in the ages.
-</li>
-<li>
-We believe in God the Father, God the son, and God the Holy Spirit.
-</li>
-<li>
-We believe in the deity, virgin birth, and bodily resurrection of Jesus Christ.
-</li>
-<li>
-We believe that salvation is by grace plus nothing and minus nothing. The conditions to salvation are repentance and faith.
-</li>
-<li>
-We believe that men are justified by faith alone and are accounted righteous before God only through the merit of our Lord and Savior Jesus Christ. Justification establishes an eternal relationship that can never be broken.
-</li>
-<li>
-We believe in the visible, personal, and premillennial return of Jesus Christ.
-</li>
-<li>
-We believe in the everlasting conscious blessedness of the saved and the everlasting conscious punishment of the lost.
-</li>
-</ol>
+	<div class="about"> <section class="faith"> 
+		<h2>Statement of Faith</h2> 
+			<ol> <li>We believe in the verbal inspiration and authority of the Scriptures. We believe that the Bible reveals God, the fall of man, the way of salvation, and God's plan and purpose in the ages. </li> 
+			  <li> We believe in God the Father, God the son, and God the Holy Spirit. </li> 
+			  <li> We believe in the deity, virgin birth, and bodily resurrection of Jesus Christ. </li> 
+			  <li> We believe that salvation is by grace plus nothing and minus nothing. The conditions to salvation are repentance and faith. </li> 
+			  <li> We believe that men are justified by faith alone and are accounted righteous before God only through the merit of our Lord and Savior Jesus Christ. Justification establishes an eternal relationship that can never be broken. </li> 
+			  <li> We believe in the visible, personal, and premillennial return of Jesus Christ. </li> 
+			  <li> We believe in the everlasting conscious blessedness of the saved and the everlasting conscious punishment of the lost. </li> 
+			</ol> 
 	</section>
-		
-	<aside class="five-gs">
-	<h2><span>5 G's</span></h2>
-<ul>
-<li>Glorify God (&quot;Love God with all your heart&quot;)</li>
-
-<li>Go share the Gospel (&quot;go... make disciples&quot;)</li>
 	
-<li>Grow in God's Word (&quot;teach them... all things&quot;)</li>
-	
-<li>Give of ourselves (&quot;love your neighbor as yourself&quot;)</li>
-	
-<li>Gather for fellowship (&quot;baptize&quot; or incorporate into fellowship)</li>
-</ul>
-
-	</aside>
-	</div>
+	<aside class="five-gs"> 
+		<h2><span>5 G's</span></h2> <ul> <li>Glorify God (&quot;Love God with all your heart&quot;)</li>
+		<li>Go share the Gospel (&quot;go... make disciples&quot;)</li>
+		<li>Grow in God's Word (&quot;teach them... all things&quot;)</li>
+		<li>Give of ourselves (&quot;love your neighbor as yourself&quot;)</li>
+		<li>Gather for fellowship (&quot;baptize&quot; or incorporate into fellowship)</li> </ul>
+	</aside> </div>
 	
 	<section class="highlights">
+	  <div class="purpose"> 
+	    <div class="feature-title"><h3>Purpose</h3>
+	    </div>
+	    <div class="feature-content">
+	      <p>We believe a great commitment to the Great Commandment and the Great Commission will grow a Great Church!</p>
+	    </div> 
+	  </div> 
 	
-	<div class="purpose">
-		<div class="feature-title"><h3>Purpose</h3></div>
-		
-		<div class="feature-content"><p>We believe a great commitment to the Great Commandment and the Great Commission will grow a Great Church!</p></div>	
+	<div class="commandment"> 
+	  <div class="feature-title">
+	    <h3>What is the Great Commandment?</h3>
+	  </div> 
+	   <div class="feature-content">
+	     <p>Jesus said: Love the Lord your God with all your heart... soul... and mind. This is the first and greatest commandment. And the second is like it: Love your neighbor as yourself. All the Law and Prophets hang on these two commandments.</p> 
+	     <p>Matthew 22:36-48</p> 
+	   </div> 
 	</div>
-	<div class="commandment">
-		<div class="feature-title"><h3>What is the Great Commandment?</h3></div>
-		<div class="feature-content">
-			<p>Jesus said:
-			Love the Lord your God with all your heart... soul... and mind. This is the first and greatest commandment. And the second is like it: Love your neighbor as yourself. All the Law and Prophets hang on these two commandments.</p>
-			<p>Matthew 22:36-48</p>
-		</div>
-		</div>
 	
-	
-	<div class="commission">
-		<div class="feature-title"><h3>What is the Great Commission?</h3></div>
-		<div class="feature-content">
-			<p>Jesus said:
-			Go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit, and teaching them to obey everything I have commanded you.</p>
-			<p>Matthew 28:19-20</p>
-		</div>
-	</div>
+	<div class="commission"> 
+	  <div class="feature-title">
+	    <h3>What is the Great Commission?</h3></div> 
+	    <div class="feature-content"> 
+	      <p>Jesus said: Go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit, and teaching them to obey everything I have commanded you.</p> 
+	      <p>Matthew 28:19-20</p> 
+	  </div> 
+	</div> 
+	    
 	</section>
 
-<footer>
-	<?php
-		include('includes/footer.php');
-	?>
-</footer>
-</div>
-</body>
+	<footer> <?php include('includes/footer.php'); ?> 
+	</footer> 
+	</div> 
+</body> 
 </html>
