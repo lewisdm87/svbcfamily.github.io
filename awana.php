c8f51542a4ee79198cb40de8e2907a9847c0a856
David M Lewis <me@dlewis.info>
2020-03-15 20:24:03 -0400

    Removed special announcement

--- awana.php
+++ awana.php
@@ -17,6 +17,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 </head>
+
 <body>
 <div class="page">
 	
@@ -25,11 +26,8 @@
 <?php
 	include('includes/header.php');
 ?>
-</header>
-<?php
-	include('includes/special.php');
-?>
 
+</header>
 	
 	<section class="title"><h1>Awana Clubs</h1></section>
 	
