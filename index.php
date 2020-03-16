511641f26cfe5a9110d9ddaa8e3a592541e42c19
David M Lewis <me@dlewis.info>
2020-03-15 21:00:04 -0400

    Formatted readability

--- index.php
+++ index.php
@@ -1,126 +1,63 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html> <html lang="en"> 
 <head>
-
-<title>HOME</title>
-	
-<meta name="Description" content="This is the main page of Shenandoah Valley Baptist Church (SVBC). SVBC is a quiet little church at the top of a hill located between Rt. 11 and I-81 just outside Stephens City. We are a non-partisan church with a focus on individual relationship with God.">
-	
-<meta name="robots" content="index, follow">
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	
-<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
-<link rel="stylesheet" href="../css/svbcfamily.css"/>
-
-<!-- Add Icons for hamburger menu and page close icon -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
+	<title>HOME</title>
+	<meta name="Description" content="This is the main page of Shenandoah Valley Baptist Church (SVBC). SVBC is a quiet little church at the top of a hill located between Rt. 11 and I-81 just outside Stephens City. We are a non-partisan church with a focus on individual relationship with God.">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="widthÞvice-width, initial-scale=1.0"/>
+	<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/> <link rel="stylesheet" href="../css/svbcfamily.css"/>
+	<!-- Add Icons for hamburger menu and page close icon --> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
 
-<body>
-<div class="page">
+<body> 
+	
+	<div class="page">
 
-<header>
-<?php
-	include('includes/header.php');
-?>
-</header>
+	<header> <?php include('includes/header.php'); ?> </header>
 
-	<div class="title">
-		<h1>Welcome to Shenandoah Valley Baptist Church:</h1>
-		<h1>Everyone is welcome!</h1>
-		<h1><i>Helping people find and become passionate about Jesus</i></h1>
+	<div class="title"> 
+		<h1>Welcome to Shenandoah Valley Baptist Church:</h1> <h1>Everyone is welcome!</h1> <h1><i>Helping people find and become passionate about Jesus</i></h1> 
 	</div>
-	
-	<div class="main">
-	  <p>We are a church in the heart of the Shenandoah Valley that is focused on the whole family. We have a place for anyone who wishes to join us in worshiping the Lord. We know you'll find a warm and welcoming home with our church family.</p>
-	
-	  <p>You can watch the morning service live on our <a class="nav" href="https://www.facebook.com/svbcfamily">FaceBook</a> page. Be sure to follow us on facebook for more information on upcoming events and daily scripture.</p>
-	
-	  <p>Videos are also uploaded to <a class ="nav" href="https://www.youtube.com/channel/UCVNMH7wzOzSwZ6l-HnoKC7g">YouTube</a> after the service. Please subscribe to our channel for future videos.</p>
-	
-	  <p>Children (Preschool - 8th Grade) are welcome to participate in the <i>Awana Program</i> that meets on Wednesday Nights <strong>September - May</strong>. (See the <a class="nav" href="awana.html">Awana Clubs Page</a> for more detail).</p>
-	</div>
-	
-	
+
+	<div class="main"> 
+		<p>We are a church in the heart of the Shenandoah Valley that is focused on the whole family. We have a place for anyone who wishes to join us in worshiping the Lord. We know you'll find a warm and welcoming home with our church family.</p>
+
+		<p>You can watch the morning service live on our <a class="nav" href="https://www.facebook.com/svbcfamily">FaceBook</a> page. Be sure to follow us on facebook for more information on upcoming events and daily scripture.</p>
+
+		<p>Videos are also uploaded to <a class ="nav" href="https://www.youtube.com/channel/UCVNMH7wzOzSwZ6l-HnoKC7g">YouTube</a> after the service. Please subscribe to our channel for future videos.</p>
+
+		<p>Children (Preschool - 8th Grade) are welcome to participate in the <i>Awana Program</i> that meets on Wednesday Nights <strong>September - May</strong>. (See the <a class="nav" href="awana.html">Awana Clubs Page</a> for more detail).</p>
+	 </div>
+
 	<div class="services">
-	  <table class="tbl-sunday">
-		<caption>Sunday Services</caption>		
-		<tr>
-			<td>9:00am</td>
-			<td>-</td>
-			<td>10:00am</td>
-			<td>Sunday School</td>
-		</tr>
-		<tr>
-			<td>10:00am</td>
-			<td>-</td>
-			<td>10:20am</td>
-			<td>Fellowship in the Gym</td>
-		</tr>
-		<tr>
-			<td>10:30am</td>
-			<td>-</td>
-			<td>11:30am</td>
-			<td>Worship Service</td>
-		</tr>
-	  </table>
-	
-	  <table class="tbl-wednesday">
-		<caption>Wednesday Nights</caption>
-		<tr>
-			<td>6:30pm</td>
-			<td>-</td>
-			<td>7:30pm</td>
-			<td>Devotion and Intentional Prayer</td>
-		</tr>
-		<tr>
-			<td>6:30pm</td>
-			<td>-</td>
-			<td>8:30pm</td>
-			<td>Awana Clubs*</td>
-		</tr>
-		<tr>
-			<td colspan="4">*Meets September - May</td>
-		</tr>
-	  </table>
+		 <table class="tbl-sunday"> <caption>Sunday Services</caption> 
+			<tr> <td>9:00am</td> <td>-</td> <td>10:00am</td> <td>Sunday School</td> </tr> 
+			<tr> <td>10:00am</td> <td>-</td> <td>10:20am</td> <td>Fellowship in the Gym</td> </tr>
+		 	<tr> <td>10:30am</td> <td>-</td> <td>11:30am</td> <td>Worship Service</td> </tr>
+		</table>
+
+		<table class="tbl-wednesday"> <caption>Wednesday Nights</caption> 
+			<tr> <td>6:30pm</td> <td>-</td> <td>7:30pm</td> <td>Devotion and Intentional Prayer</td> </tr> 
+			<tr> <td>6:30pm</td> <td>-</td> <td>8:30pm</td> <td>Awana Clubs*</td> </tr> 
+			<tr> <td colspan="4">*Meets September - May</td> </tr>
+		 </table> 
 	</div>
-	
+
 	<div class="main">
-	  <div class="main-left">
-	    <h2>From the Pastor</h2>
-	    <p> Thank you so much for visiting the Web Page of Shenandoah Valley Baptist Church. We would enjoy the opportunity to meet you and your family so I want to personally invite you to come "kick the tires" so to speak and join us for a time of learning, fellowship, and worship. Should you formulate any questions as you peruse this site feel free to contact us through our contact page. I look forward to personally meeting you.</p>
-	
-	  <p>Pastor Don & Debbie Cherry</p>
-	  <p>SVBC</p>
-	  <figure>
-		<img src="media/people/don-debbie_cherry.jpg" alt="pastor-cherry" style="width:100%">
-		<figcaption>Pastor Don Cherry with wife Debbie</figcaption>
-	  </figure>
+
+	 <div class="main-left"> <h2>From the Pastor</h2>
+		 <p> Thank you so much for visiting the Web Page of Shenandoah Valley Baptist Church. We would enjoy the opportunity to meet you and your family so I want to personally invite you to come "kick the tires" so to speak and join us for a time of learning, fellowship, and worship. Should you formulate any questions as you peruse this site feel free to contact us through our contact page. I look forward to personally meeting you.</p>
+		<p>Pastor Don & Debbie Cherry</p> <p>SVBC</p>
+ 		<figure> <img src="media/people/don-debbie_cherry.jpg" alt="pastor-cherry" style="width:100%"> <figcaption>Pastor Don Cherry with wife Debbie</figcaption> </figure> 
 	</div>
-	
-	  <div class="main-right">
-	    <h2>Praise & Worship Leader</h2>
-	    <p>Garrett and his wife Nicole are recent graduates of WVU who now live in the Martinsburg (WV) area. Both WV natives, Garrett has been involved in praise teams both in High School and college. We are grateful to have Garrett lead our time of praise and so appreciate he and Nicole's commitment here at SVBC.</p>
-	    <figure>
-		  <img src="media/people/garret-nichole_sheppard.png" alt="garrett" style="width:100%">
-		  <figcaption>Garrett and wife Nicole</figcaption>
-	    </figure>
-	  </div>
-	</div>
-	
-	
 
-<footer>
-	<?php
-		include('includes/footer.php');
-	?>
-</footer>
+	<div class="main-right"> <h2>Praise & Worship Leader</h2> 
+		<p>Garrett and his wife Nicole are recent graduates of WVU who now live in the Martinsburg (WV) area. Both WV natives, Garrett has been involved in praise teams both in High School and college. We are grateful to have Garrett lead our time of praise and so appreciate he and Nicole's commitment here at SVBC.</p> 
+		<figure> <img src="media/people/garret-nichole_sheppard.png" alt="garrett" style="width:100%"> <figcaption>Garrett and wife Nicole</figcaption> </figure> 
+	</div> 
+</div>
 
+<footer> <?php include('includes/footer.php'); ?> </footer>
 
-
-
-	</div>
-</body>
+</div>
+</body> 
 </html>
