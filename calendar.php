08c2e21f83d82f40a4a6bccddbc541edb84f269c
David M Lewis <me@dlewis.info>
2020-03-17 20:45:12 -0400

    Format

--- calendar.php
+++ calendar.php
@@ -1,38 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-<title>CALENDAR</title>
-	
-<meta name="Description" content="Want to see what we're all about before joining us for services? That's okay, most of our events are open to the public and we welcome anyone to join us for our highlight events.">
-
-<meta name="robots" content="index, follow">
-	
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	
-<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
-<link rel="stylesheet" href="../css/svbcfamily.css"/>
-
-<!-- Add Icons for hamburger menu and page close icon -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
-</head>
+	<title>CALENDAR</title>
+	<meta name="Description" content="Want to see what we're all about before joining us for services? That's okay, most of our events are open to the public and we welcome anyone to join us for our highlight events.">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/><link rel="stylesheet" href="../css/svbcfamily.css"/>
+	<!-- Add Icons for hamburger menu and page close icon -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
 
 	<?php include('includes/header.php'); ?> 
 
 <div class="g-cal">
 	<h2>Scheduled Services</h2>
-<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=YWhtdWxwOGpiamlhZ3RzNXZnMXBpc2Vpa2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23009688&amp;color=%230B8043&amp;mode=AGENDA" style="border-width:0" width="960" height="600"></iframe>
+	<iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=YWhtdWxwOGpiamlhZ3RzNXZnMXBpc2Vpa2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23009688&amp;color=%230B8043&amp;mode=AGENDA" style="border-width:0" width="960" height="600"></iframe>
 </div>
 
 <footer>
 	<?php
 		include('includes/footer.php');
-	?></footer>
+	?>
+</footer>
 	
-	</div>
-
-</body>
-</html>
+</div> </body> </html>
