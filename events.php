c4c80655b89e4b345a6901cd15859f28b7ad87de
David M Lewis <me@dlewis.info>
2020-03-17 20:45:17 -0400

    Format

--- events.php
+++ events.php
@@ -1,42 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html> <html lang="en"> 
 <head>
-
-<title>EVENTS</title>
-	
-<meta name="Description" content="We believe that church should be about more than listening to a sermon on Sunday. To help grow in Christ come out and be involved! There's something for everyone here at SVBC.">
-
-<meta name="robots" content="index, follow">
-	
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	
-<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
-<link rel="stylesheet" href="../css/svbcfamily.css"/>
-
-<!-- Add Icons for hamburger menu and page close icon -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
+	<title>EVENTS</title>
+	<meta name="Description" content="We believe that church should be about more than listening to a sermon on Sunday. To help grow in Christ come out and be involved! There's something for everyone here at SVBC.">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="widthÞvice-width, initial-scale=1.0"/>
+	<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/> <link rel="stylesheet" href="../css/svbcfamily.css"/>
+	<!-- Add Icons for hamburger menu and page close icon --> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
 
-	<?php include('includes/header.php'); ?> 
+<?php include('includes/header.php'); ?>
 
-<?php
-	include('events/movie-night.html');
-?>
+<?php include('events/movie-night.html'); ?>
 
 <vr>
 
-<?php
-	include('events/bikes.html');
-?>
+<?php include('events/bikes.html'); ?>
 
-<footer>
-<?php
-	include('includes/footer.php');
-?>
-</footer>
+<footer> <?php include('includes/footer.php'); ?> </footer>
 
-</div>
-
-</body>
-</html>
+</div> </body> </html>
