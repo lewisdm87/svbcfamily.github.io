e30310fa998a3e4c9b832e333b78fbe5d7be8663
David M Lewis <me@dlewis.info>
2020-03-17 20:25:02 -0400

    Page edits

--- includes/special.php
+++ includes/special.php
@@ -6,6 +6,7 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<p>Just a simple test</p>
+	<p>Prayer group and Awana Clubs cancelled for We. 3/18.</p>
+	<p>Updated information will be posted on social @svbcfamily and website as available.</p>
 </body>
 </html>
