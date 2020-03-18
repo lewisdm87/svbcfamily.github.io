1d1ef513808354ab9696c606d27da4afc0d4c3db
David M Lewis <me@dlewis.info>
2020-03-17 20:24:46 -0400

    Page edits

--- calendar.php
+++ calendar.php
@@ -18,37 +18,14 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 </head>
-<body>
-<div class="page">
-	
+</head>
 
-<header>
-<?php
-	include('includes/header.php');
-?>
-</header>
-
-
+	<?php include('includes/header.php'); ?> 
 
 <div class="g-cal">
 	<h2>Scheduled Services</h2>
 <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=YWhtdWxwOGpiamlhZ3RzNXZnMXBpc2Vpa2dAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23009688&amp;color=%230B8043&amp;mode=AGENDA" style="border-width:0" width="960" height="600"></iframe>
 </div>
-
-<section class="final">	
-	  <div class="bottom-contact">
-		<h2><span style="text-decoration: overline">CONTACT INFO</span></h2>
-		<br>
-		<p>4699 Valley Pike</p>
-		<p>Stephens City, VA 22655</p>
-		<br>
-		<p>Office: 540-868-4020</p>
-		<p>Fax: 540-869-4679</p>
-		<br>
-		<a href="mailto:info@svbcfamily.com" title="Send Us an E-Mail">info@svbcfamily.com</a>
-	</div>
-		
-</section>
 
 <footer>
 	<?php
