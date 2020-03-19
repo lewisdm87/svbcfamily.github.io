a6112c77439a10533f7967c2bd9b2f6bfcf630a2
David M Lewis <me@dlewis.info>
2020-03-19 16:48:25 -0400

    Modified special updates

---
+++ status.php
@@ -0,0 +1,57 @@
+<?php include('includes/header.php'); ?> 
+
+<div class="title">
+<h1>Current Notes on Operating Status</h1>
+
+</div>
+
+<div class="main">
+	
+<h2>March 19, 2020</h2>
+
+<p>In accordance with governmental guidlines the leadership staff at SVBC has decided on
+the following:</p>
+
+<ul>
+<li>All scheduled events for the remainder of March have been cancelled.</li>
+<li>Sunday and Wednesday night services will not be held.</li>
+<li>The leadership team will be meeting again in April to determine operating status.</li>
+</ul>
+
+<h3>Sunday Mornings</h3>
+
+<p>The pastor will open the church Sunday Mornings from 10am - 11am for anyone who wishes
+to come in and pray. The time of prayer is strictly voluntary and prayer groups will be
+limited to a maximum of 10 persons. Please remember that government guidance includes
+distances of approx. 6ft between people and to refrain from contact. It&#8217;s not being rude,
+it&#8217;s being safe.</p>
+
+<h3>Online Devotionals</h3>
+
+<p>We are exploring options for Pastor Cherry to host online devotionals Sunday mornings and
+Wednesday evenings. They will be short videos on Facebook Live and recordings will be
+transfered to our YouTube channel for your convenience. Please be sure to follow our social
+media channels and this page for updated information as it becomes available.</p>
+
+<h3>Questions - Comments - Concerns</h3>
+
+<p>Connie will continue to hold regular office hours at SVBC and can be reached on the church
+phone at <a class="nav" href="tel: 540-868-4020">540&#8211;868&#8211;4020</a> or her cell. You can also reach her
+by email at <a class="nav" href="mailto: csales@svbcfamily.com">csales@svbcfamily.com</a>.</p>
+
+<p>You can reach the pastor direct via email at <a class="nav" href="mailto: pastor@svbcfamily.com">pastor@svbcfamily.com</a>.</p>
+
+<p>Or you can contact your deacon to have your concerns addressed by the appropriate persons.</p>
+
+<h3>Tithes and Offerings</h3>
+
+<p>While we are currently exploring online giving options, please be faithful in your givings.
+Sadly, the corona virus is not postponing the bills.</p>
+
+<h3>Further Updates and Guidance</h3>
+
+<p>Updates and guidance will continue to be made available to you on all of our social media
+and this page as it becomes available.</p>
+</div>
+
+<?php include('includes/footer.php') ?>
