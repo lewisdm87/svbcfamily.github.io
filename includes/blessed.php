43a48995970b8b6bd9c8f9289bd963b2b2b47a94
David M Lewis <me@dlewis.info>
2020-03-22 16:22:57 -0400

    Added be a blessing

---
+++ includes/blessed.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> <html lang="en"> 
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Special Announcement</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<a href="blessing.php">CLICK HERE TO READ HOW TO BE A BLESSING</a>
+</body>
+</html>
