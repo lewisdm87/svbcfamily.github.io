11019f75f2d1135da346b1466203c8ed708e0788
David M Lewis <me@dlewis.info>
2020-03-22 16:21:28 -0400

    Added be a blessing

---
+++ blessing.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<title>BLESSING</title>
+	
+	<meta name="robots" content="index, follow">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>	
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	
+<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/>
+<link rel="stylesheet" href="../css/svbcfamily.css"/>
+
+<!-- Add Icons for hamburger menu and page close icon -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+</head>
+
+	<?php include('includes/header.php'); ?> 
+	
+<div class="content-slides">
+
+<div class="container">
+  <h2>Be a Blessing</h2>  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="media/blessing/blessing-01.jpg" alt="title" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="media/blessing/blessing-02.jpg" alt="elderly" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="media/blessing/blessing-03.jpg" alt="neighbors" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/blessing/blessing-04.jpg" alt="service personnel" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/blessing/blessing-05.jpg" alt="single moms" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/blessing/blessing-06.jpg" alt="small business" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/blessing/blessing-07.jpg" alt="medical pro's" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/blessing/blessing-08.jpg" alt="schools" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/blessing/blessing-09.jpg" alt="homeless" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/blessing/blessing-10.jpg" alt="red cross" style="width:100%;">
+      </div>
+		
+		 <div class="item">
+        <img src="media/blessing/blessing-11.jpg" alt="prayer" style="width:100%;">
+      </div>
+		
+	
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+<p>
+	<a href="https://www.svbcfamily.com/includes/blessing-text.html" class="nav">Click Here for Text Version</a>
+</p>
+</div>
+	
+	
+<footer>
+	<?php include('includes/footer.php'); ?>	
+</footer>
+
+</div> </body> </html>
