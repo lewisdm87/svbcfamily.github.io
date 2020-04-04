2c3f0b65d202e97e190239c0127ed2c3ff284221
David M Lewis <me@dlewis.info>
2020-04-04 15:25:51 -0400

    Added PayPal

---
+++ thank-you.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html> <html lang="en"> 
+<head>
+	<title>THANK-YOU</title>
+	<meta name="Description" content="Page was created to thank all people who choose to contribute.">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="widthÞvice-width, initial-scale=1.0"/>
+	<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/> 
+	<link rel="stylesheet" href="style.css"/>
+	<!-- Add Icons for hamburger menu and page close icon --> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+
+<?php include('includes/header.php'); ?>
+
+
+<h1>Thank You</h1>
+
+<p>Shenandoah Valley Baptist Church thanks you for your faithful giving. </p>
+
+<blockquote>
+<p>Honor the Lord with your wealth and with the first fruits of all your produce; then your barns will be filled with plenty, and your vats will be bursting with wine.  Proverbs 3:9-10</p>
+</blockquote>
+
+
