4c2315208f27ed395e680a35ef6ab2b16e682c4e
David M Lewis <me@dlewis.info>
2020-04-15 20:30:36 -0400

    Removed prayer gathering and Easter Sunday.

--- events.php
+++ events.php
@@ -10,44 +10,7 @@
 
 <?php include('includes/header.php'); ?>
 
-<h2>Drive-In Prayer Gathering</h2>
-
-<h3>Thursday April 09, 7-7:30pm</h3>
-
-<p><strong>Winchester Medical Center</strong></p>
-
-<p>a. Meet at 7PM in the Green or Gray parking lots</p>
-
-<p>b. Park in a space</p>
-
-<p>c. Stay in your cars</p>
-
-<p>d. Turn on your emergency flashers</p>
-
-<p>e. Pray using the following guide:</p>
-
-<ul>
-	<li>*Pray for those who currently have the virus whether at home or in the hospital</li>
-	<li>*Pray for doctors, nurses respiratory therapist and all support staff for wisdom and protection.</li>
-	<li>*Pray for support staff, housekeepers, dietitians, patient transporters, etc for protection from the virus.</li>
-	<li>*Pray for Valley Health leadership as they make decisions and lead in these challenging times.</li>
-	<li>*Pray for hospital chaplains to have God&#39;s grace and peace in ministering to the sick, hurting and grieving.</li>
-</ul>
-
-<p>f. Please do not honk your horns</p>
-
-<p><em>This event is being led by the Calvary Baptist Church of Winchester in conjunction with the northern Va Southern Baptist Convention and has received the blessing of the WMC administration.</em></p>
-
-
-
-<hr>
-
-<h2>Easter Sunday Drive-In Service</h2>
-
-<p> On Easter Sunday (April 12, 2020) SVBC will be hosting a drive-in service.
-	Drive around park on the 81 side of the building and we will let you know what radio channel the 
-	message will be broadcast on. Please remember to practice social distancing. More details to follow.
-</p>
+<?php include('events/no-events.html'); ?>
 
 <footer> <?php include('includes/footer.php'); ?> </footer>
 
