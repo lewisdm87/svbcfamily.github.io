21e8d0a2c2bdc098c99ae0d6a22e7cc2e85930df
David M Lewis <me@dlewis.info>
2020-05-31 13:30:31 -0400

    New page: email updates

---
+++ updates.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+
+<head>
+	<title>STATUS</title>
+	<meta name="Description" content="Answering questions about Shenandoah Valley Baptist Church operating status during the covid-19
+		epidemic. Are we open? What are the hours? Should we be tithing? Is the live stream being produced?">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="widthÞvice-width, initial-scale=1.0"/>
+	<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/> <link rel="stylesheet" href="../css/svbcfamily.css"/>
+	<!-- Add Icons for hamburger menu and page close icon --> <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+
+
+<?php include('includes/header.php'); ?> 
+
+<div class="title">
+<h1>Email Updates</h1>
+
+</div>
+
+<div class="main">
+	
+<h2>May 30, 2020</h2>
+
+<p>Subject: Update</p>
+
+<p>Mr. Sandford made it through the night!! They are warming him up which will take all day until 7: 30 PM. Please keep praying!!</p>
+
+
+<hr>
+
+<h2>May 29, 2020</h2>
+
+<p>Subject: Urgent prayer need</p>
+
+<p>Please pray for Mr. Sanford who is battling the Covid virus. He just went into cardiac arrest! They were able to revive him but its not good. His son Sanford Jr. was able to suit up and go in to see him and pray for him. He did recognize his son. So please pray !</p>
+
+<p>Also pray for Margaret Sandretzky during your daily prayers. She is also battling a health condition. Having her ups and downs lately with more downs. She ask for your prayers.</p>
+
+<p> Prayers please</p>
+
+
+<hr>
+
+<h2>April 25, 2020</h2>
+
+<p>Subject: Update</p>
+
+<p>Larry Williams has passed away. His family was told yesterday that he was brain dead. They were going this morning to take him off the machine. Please pray for this family. </p>
+
+<p>The address if you would like to send a card is Williams Family ( Kim ,Wife&#39;s name) is</p>
+
+<p>832 Poorhouse Road, Winchester, Va. 22603</p>
+
+<p>Putting a new roof on our school building was the last job they were working on.</p>
+
+<p>Prayers</p>
+
+
+<hr>
+
+<h2>April 4, 2020</h2>
+
+<p>Subject: Sunday</p>
+
+<p>The church will be open tomorrow from 10 am to 11 am for anyone that wants to come and pray. No sermon but doors will be open at that time.</p>
+
+<p>Blessings</p>
+
+
+<hr>
+
+
+</div>
+
+<?php include('includes/footer.php') ?>
