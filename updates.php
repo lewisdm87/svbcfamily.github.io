c2a47a3b2c6feefe9fbc89b8addaa27ece38ecea
David M Lewis <me@dlewis.info>
2020-05-31 14:47:33 -0400

    Edits 20200531

--- updates.php
+++ updates.php
@@ -2,9 +2,8 @@
 <html lang="en"> 
 
 <head>
-	<title>STATUS</title>
-	<meta name="Description" content="Answering questions about Shenandoah Valley Baptist Church operating status during the covid-19
-		epidemic. Are we open? What are the hours? Should we be tithing? Is the live stream being produced?">
+	<title>UPDATES</title>
+	<meta name="Description" content="Email updates sent out to SVBC.">
 	<meta name="robots" content="index, follow">
 	<meta name="viewport" content="widthÞvice-width, initial-scale=1.0"/>
 	<link rel="shortcut icon" href="../media/favicon.ico" type="image/x-icon"/> <link rel="stylesheet" href="../css/svbcfamily.css"/>
@@ -19,7 +18,7 @@
 
 </div>
 
-<div class="main">
+<div>
 	
 <h2>May 30, 2020</h2>
 
@@ -29,6 +28,8 @@
 
 
 <hr>
+</div>
+<div>
 
 <h2>May 29, 2020</h2>
 
@@ -42,36 +43,252 @@
 
 
 <hr>
-
-<h2>April 25, 2020</h2>
-
-<p>Subject: Update</p>
-
-<p>Larry Williams has passed away. His family was told yesterday that he was brain dead. They were going this morning to take him off the machine. Please pray for this family. </p>
-
-<p>The address if you would like to send a card is Williams Family ( Kim ,Wife&#39;s name) is</p>
-
-<p>832 Poorhouse Road, Winchester, Va. 22603</p>
-
-<p>Putting a new roof on our school building was the last job they were working on.</p>
-
-<p>Prayers</p>
-
-
-<hr>
-
-<h2>April 4, 2020</h2>
-
-<p>Subject: Sunday</p>
-
-<p>The church will be open tomorrow from 10 am to 11 am for anyone that wants to come and pray. No sermon but doors will be open at that time.</p>
-
-<p>Blessings</p>
-
-
-<hr>
-
-
 </div>
+
+<div>
+
+<h2>May 28, 2020</h2>
+
+<p>Subject: Pastor’s Devotional</p>
+
+<p>THE WISDOM OF GOD</p>
+
+<p>“Consider the work of God, for who is able to straighten what he has bent?” - Eccl 7:13</p>
+
+<p>The picture is one of bare hands trying to straighten curved steel. What sweat and tears have you lost on trying to straighten out your circumstances? What temples have you rubbed raw trying to figure out how God is going to use this dreadful experience? Surely God can’t know about this situation. Doesn’t He see how crooked my life is? Why doesn’t He straighten it out?</p>
+
+<p>This week we will learn why living in “to be continued” land is not only a necessity, but actually the best thing for us. We will be introduced to another of God’s attributes: His wisdom. And that’s not about God’s IQ. When stuck in your next unresolved mystery remember these principles on how we can learn to rest and trust in the wisdom of God.</p>
+
+<p>I. The wisdom of God: God brings about the best possible ends, by the best possible means, for the most possible people, for the longest possible time.</p>
+
+<ul>
+	<li>What is the most difficult issue in your life?
+
+		<ul>
+			<li>What big problem do you want God to solve?</li>
+		</ul></li>
+</ul>
+
+<p>II. How has God revealed His wisdom?</p>
+
+<ul>
+	<li>Creation
+
+		<ul>
+			<li>Providence</li>
+			<li>Redemption</li>
+			<li>Jesus</li>
+		</ul></li>
+</ul>
+
+<p>“Be very careful then, how you live - not as unwise but as wise, making the most of every opportunity, because the days are evil. Therefore, do not break foolish, but understand what the Lord’s will is.” - Eph 5:15-17</p>
+
+<p>III. Ways to live a wise life:</p>
+
+<ul>
+	<li>Wise living begins with reverence for God. Reverence = in awe of…
+
+		<ul>
+			<li>Wise living grows by receiving His word - Listening &amp; applying </li>
+			<li>Wise living requires that we ask for it specifically</li>
+			<li>Wise living involves learning to trust God completely</li>
+		</ul></li>
+</ul>
+
+<p>Questions:</p>
+
+<ol>
+	<li>What is the toughest issue you’re facing in your life right now?</li>
+	<li>What did you learn about God’s wisdom that gave you hope or comfort?</li>
+</ol>
+
+<p>Happy Studying</p>
+
+
+<hr></div>
+
+<div>
+<h2>May 26, 2020</h2>
+
+<p>Subject: Prayer Meeting !!</p>
+
+<p>Prayer group re-starts tomorrow at 6:30pm! Wednesday May 27, 2020!</p>
+
+
+<hr>
+</div>
+
+<div>
+<h2>May 26, 2020</h2>
+
+<p>Subject: Urgent Prayer Need</p>
+
+<p>Mr. Sanford Silver took a turn for the worse yesterday evening. Please pray for him! Will update when I get an update. Thank you</p>
+
+
+<hr>
+</div>
+
+
+<div>
+<h2>May 22, 2020</h2>
+
+<p>Subject: Clean up Day, Update, Sunday</p>
+
+<p>Tomorrow Saturday May 23, 2020 is Clean up Day !&nbsp; 9 a.m. - ? We are getting our church ready for Service on Sunday May 24, 2020 ! A lot has already been done, but there is still a lot to do. It&#39;s a great time for Fellowship and just taking care of God&#39;s Church and Property ! There will be lunch provided, for we know after working all morning we need a little rest and energy for the next couple of hours. You don&#39;t have to stay for lunch or if you don&#39;t want lunch and want to keep on working that&#39;s great! The important thing is serving our Lord and Savior!</p>
+
+<p>You will be Blessed!&nbsp;</p>
+
+<p>Mr. Sanford&nbsp;Silver, Sr. is off the ventilator&nbsp;and breathing on his own. He is getting better each day but still has a while to recover. He is still in the hospital, hopefully he will be back home soon. Please keep praying for him! It would be wonderful if we fill his mailbox up with get well and encouragement cards. I know that will give him a real boost. He loves this church and the church family so here is his address: 3605 Valley Ave. Winchester, VA 22602....lets be a Blessing to him!&nbsp;</p>
+
+<p>Hope to see everyone Sunday at church. We will still have the drive in Service for ones who don&#39;t want to come inside at this time. The whole place is and will be clean and sanitized!&nbsp;</p>
+
+<p>My Scripture for today</p>
+
+<p>For God hath not given us the spirit of fear; but of power, and of love and of a sound mind.&nbsp; 11 Timothy&nbsp;1:7</p>
+
+
+<hr>
+</div>
+
+<div>
+<h2>May 21, 2020</h2>
+
+<p>Subject: Saturday Cleanup Day!</p>
+
+<p>This Saturday May 23, 2020 we are having cleanup day from 9 a.m. - ? Lunch will be provided. If you don&#39;t want to stay for lunch that&#39;s fine. We appreciate anytime you can give. We are getting the church ready for Sunday! Some have been up this week to clean and do work on the property with not being able to come on Saturday. This is a great time to serve.</p>
+
+<p>The Sanctuary&nbsp;is clean and sterilized! There will be other projects in the building and outside on the property, If you can give an hour or more that will be great. If you have certain things you clean with or certain products you like to use please bring them. We will have cleaning products here for you to use also.</p>
+
+<p>We need a Power Washer&nbsp;is anyone has one!</p>
+
+<p>Sunday you are not required to wear a mask or gloves. We will have masks if you do not have one and want to wear it doing church time. There will be gel hand sanitizer&nbsp;at the door also.</p>
+
+<p>There will be a box as you enter the building for your Tithes and Offerings since we cannot pass the plate. The box on each side is yellow with a bow.</p>
+
+<p>Many Blessings</p>
+
+
+<hr>
+</div>
+
+<div>
+<h2>May 13, 2020</h2>
+
+<p>Subject: prayer needs</p>
+
+<p>Mr, Sanford&nbsp;Silver Sr, is still in the hospital with the virus. Last night there was no change, He is on a ventilator. Please keep praying for him. He is a regular attender at our church.</p>
+
+<p>Richard Palmer is in need of a vehicle. If you know of one or can lead him in the right direction please call him at 540-247-1005. It just needs to run and in decent shape mechanically. He doesn&#39;t have much money so it would have to be cheap.&nbsp;</p>
+
+
+<hr>
+</div>
+
+<div>
+<h2>May 11, 2020</h2>
+
+<p>Subject: Prayer Urgent</p>
+
+<p>Mr. Sandford Silver,Sr. was put on a ventilator early this morning. Please pray for him! </p>
+
+
+<hr>
+</div>
+
+<div>
+<h2>May 08, 2020</h2>
+
+<p>Subject: Prayer needs</p>
+
+<p>Mr. Sanford the older gentleman that attends our church is in WMC with the virus. His son Sanford that does our mowing and uses the gym called to ask prayer for him. I cannot get a room number for him at this time. Please pray for him.</p>
+
+<p>Marie Heironimus who is at the Willows is needing someone to take her to doctor visits a couple times a month. Her daughter is going to get me a schedule. Please think about it if you may be able to do one a month or so. Her daughter does not live around here and they don&#39;t have anyone else. Marie and Mitch were regular attenders until their health issues got them down. They are both at the Willows. Mitch has Alzheimer&#39;s&nbsp;and is in a separate&nbsp;unit but Marie has meals with him when she can.&nbsp;</p>
+
+<p>Have a wonderful and safe week-end. Hope to see you at our Drive in Sunday Service. Its been great and a wonderful blessing! You are missing a blessing if you haven&#39;t come. God&#39;s word is still being preached and we are singing in our vehicles and Praising God that we can do this!&nbsp; So come on out and join your church family this Sunday. We miss you.</p>
+
+<p>Isaiah 41:10</p>
+
+<p>Fear thou not, for I am with thee; be not dismayed; for I am thy God: I will strengthen thee; yea I will help thee; yea I will uphold thee with&nbsp;the right hand of my&nbsp;righteousness</p>
+
+
+<hr>
+</div>
+
+<div>
+<h2>May 07, 2020</h2>
+
+<p>Subject: prayer need</p>
+
+<p>Richard Palmer needs a vehicle. His caught on fire and is not fixable. If anyone knows where he can get one that is very reasonable and good running condition please call him at 540-247-1005.&nbsp;</p>
+
+
+<hr>
+</div>
+
+<div>
+<h2>May 07, 2020</h2>
+
+<p>Subject: Pastor’s Devotional</p>
+
+<p>THE GOODNESS OF GOD</p>
+
+<p>Ps 84:11</p>
+
+<p>Our world today seems to see God through the lens of circumstance. They tend to look at the violence, disease, cruelty, etc and state: “If God is too good, why do these things happen.” So, how would you answer that question? The explanation you are going to give is going to be based on how you see God in your own experience; how God has revealed himself to you in your life. Listen to the words and wisdom of A.W. Tozer:</p>
+
+<p>“The goodness of God is that which disposes Him to be kind, cordial, benevolent, and full of good will toward men. He is tenderhearted and quick of sympathy, and His unfailing attitude toward all moral beings is open, frank, and friendly. By His nature, He is inclined to bestow blessedness and He takes holy pleasure in the happiness of His people.”</p>
+
+<p>QUES: How has God revealed His goodness towards you?</p>
+
+<p>1.</p>
+
+<p>2.</p>
+
+<p>3.</p>
+
+<p>4.</p>
+
+<p>If your list is like mine it is often circumstantial (health, means, reliable vehicle, etc). God’s goodness however, is to be based on at least two truths:</p>
+
+<ol>
+	<li>His character - Just, holy, loving, impartial, righteous, etc.
+
+		<ol start="2">
+			<li>His plan for you and me - Listen to Tozer once again:
+
+				<p>“With the goodness of god to desire our highest welfare, the wisdom of God to plan it, and the power of God to achieve it, what do we lack?”</p></li>
+		</ol></li>
+</ol>
+
+<p>You see folks, in this journey we call life God uses all circumstances, some of which He specifically implements, to “conform us to the image of His Son.” Our Father is always chiseling, honing and shaping us to be more like Jesus. Just the fact that He sees, care’s and has a plan for my life is indeed what reveals that He is a good, good God.</p>
+
+<p>“For the Lord God is a sun and shield: The Lord will give grace and glory: no good thing will He withhold from them that walk uprightly.” Ps 84:11</p>
+
+<p>Happy Studying</p>
+
+
+<hr>
+</div>
+
+<div>
+<h2>May 05, 2020</h2>
+
+<p>Subject: Praise and Prayer, Williams and Hensley Family’s</p>
+
+<p>Pastor Eric Farel was released from the hospital and is recovering at home. He did have pneumonia. If you would like to send him a get well or praying for you card his address is 366 Gough Road, Winchester, Va 22602</p>
+
+<p>The church family gave two&nbsp; beautiful tropical plants to Kim Williams &amp; Family. Her husband Larry was doing the roof at our school building which he had been taking care of it until we were able to put a roof on. She loves Tropical Plants and it made her very happy. She appreciated the thoughtfulness&nbsp;of our church family. 832 Poorhouse Road, Winchester, VA 22603- if you would like to send Kim a card.&nbsp;</p>
+
+<p>We will be doing something for Amanda Hensley family very soon.&nbsp;</p>
+
+
+<hr>
+</div>
+
+
+
+
+
 
 <?php include('includes/footer.php') ?>
