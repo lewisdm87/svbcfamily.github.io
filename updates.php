104181f62581b0dfe07bc77b0abaa24f0df67a24
David M Lewis <me@dlewis.info>
2020-06-11 18:07:15 -0400

    Update 20200611

--- updates.php
+++ updates.php
@@ -19,6 +19,59 @@
 </div>
 
 <div>
+<h2>June 11, 2020</h2>
+
+<p>Subject: Pastor’s Devotional</p>
+
+<p>GOD…As He longs for you to see Him</p>
+
+<p>The Love of God</p>
+
+<p>You know what happens to soda when you leave it sitting open?….it becomes flat! Has the word love become flat in our cultural vernacular? After all we use it to describe our affection for anything from our families to hot dogs. Sadly, those of us who have been in church for any length of time, the mantra ‘God loves you’ moved from humbling to humdrum years ago. God wants to move us from flat to flavorful, moving His love for us from our minds to our hearts. Coming to understand Gods love for us will go a long way in truly loving others; even those we seriously disagree with.</p>
+
+<p>I.Five distinct implication of God’s love</p>
+
+<p>A. God’s thoughts, intentions, desires and plans area always for your good and never for your harm. Her 29:11, James 1:17</p>
+
+<p>B. God is kind, open, approachable, frank, and eager to be your friend. John 13:12-15</p>
+
+<p>C. God emotionally identifies with your pain, joy, hopes, and dreams and has chosen to allow your happiness to affect His own. John 11:33-36</p>
+
+<p>D. He takes pleasure in you just for who you are totally apart from your performance and/or accomplishments. Ps 139, Zeph 3, Rom 5:8</p>
+
+<p>E. God is actively and creatively orchestrating people, circumstances, and events to express His affection and selective correction to provide for your highest good.</p>
+
+<blockquote>
+<p>”The Lord your God is with you. He is mighty to save. He will take great delight in you, He will quiet you with His love, He will rejoice over you with singing” - Zeph 3:17</p>
+</blockquote>
+
+<p>II. Definition of God love,</p>
+
+<p>God’s love is His holy disposition toward all that He has created that compels Him to express unconditional affection and selective correction to provide the highest quality of existence, both now and forever, for the object of His love.</p>
+
+<p>III.How do we respond to Gods love?</p>
+
+<p>A. We must receive</p>
+
+<p>B. We must believe</p>
+
+<p>IV.Three ways to take the love of God from your head down into your heart.</p>
+
+<p>A. Renew your mind</p>
+
+<p>B. Pray and ask God to help</p>
+
+<p>C. Refuse cheap substitutes</p>
+
+<p>Question: What specific steps of faith/obedience are you going to take to help you experience and enjoy God’s unconditional love for you/</p>
+
+<p>Happy Studying</p>
+
+
+<hr>
+</div>
+
+<div>
 <h2>June 10, 2020</h2>
 
 <p>Subject: Update and new request</p>
