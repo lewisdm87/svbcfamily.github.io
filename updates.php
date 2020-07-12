da07bf084a17d18d4a66d799c7d37fe7290cef9a
David M Lewis <me@dlewis.info>
2020-07-12 13:26:57 -0400

    Updated 20200712

--- updates.php
+++ updates.php
@@ -18,6 +18,21 @@
 </div>
 
 <div>
+<h2>July 12, 2020</h2>
+
+<p>Subject - prayer needs and security meeting</p>
+
+<p>Be in prayer for Garrett and Nicole as they head back to WV. Garrett’s grandfather had a massive heart attack this past week and the family has been called together to make the decision as to whether or not to disconnect him from the life sustaining equipment. Pray for God’s peace and comfort for the Sheppard family at this time.</p>
+
+<p>A brief gathering will be held after the service tomorrow for those who have taken the security training in regards to forming a security team at SVBC. Those who attended along with those who would be interested in taking part are encouraged to attend as we decide when to hold a formal meeting ASAP to go over procedures and scheduling.</p>
+
+<p>Please pray for Dallas Hartley and Carolyn as Dallas has been having some serious issues and had to be taken to the hospital today after the second fall this morning. He has been falling a lot. He also has stage 4 kidney failure. So pray that they will be able to get some rest and that this can be resolved in some way. (Connie Sales sister &amp; brother-in-law)</p>
+
+
+<hr>
+</div>
+
+<div>
 <h2>July 09, 2020</h2>
 
 <p>Subject: Pastor’s Devotional</p>
@@ -180,100 +195,6 @@
 
 <hr>
 </div>
-
-<div>
-<h2>June 24, 2020</h2>
-
-<p>Subject: Pastor’s Devotional</p>
-
-<p>THE FIVE SIGNS OF LIFE...Back to the basics of authentic Christianity </p>
-
-<p><em>by Dr. David Jeremiah</em></p>
-
-<p>When a patient is brought into a hospital emergency room, he is immediately checked for &quot;vital signs&quot; of life. We could do the same check on the body of Christ. If we are not manifesting the same signs of life in our lives that Christ did, our world will never see Him as they should.</p>
-
-<p>Just as there should be signs of life if someone is physically alive, so there should be signs of spiritual life as well — signs of spiritual life in those who profess to be followers of the Lord Jesus Christ. There are some signs of life that are recognized in the Christian community signs such as worship, Bible reading, giving, the fruit of the Spirit, and others. But there should also be signs of life that are recognized</p>
-
-<p>outside the church — in the marketplace of this world. Jesus Christ was certainly recognized for the spiritual life that He manifested. And those that follow Him should also manifest clear signs of life.</p>
-
-<p>I. Dusty Shoes — Living a relevant life — Matt 5:13-16</p>
-
-<p> A. We are the Salt of the Earth:</p>
-
-<ol>
-	<li>1. Salt prevents Decay:</li>
-
-		<p> • Left to itself, without outside efforts to preserve it, the world would decompose spiritually. The &quot;salt&quot; that God uses to preserve the world while the Gospel is being spread is the body of Christ — individual believers being &quot;salty&quot; in the world.</p>
-
-		<p> • Believers are to be the voice for life, marriage, home, character, hope—Jesus!</p>
-
-		<ol start="2">
-			<li>2. Salt provides flavor:</li>
-
-				<p> • Christ in us provides flavor in an unsavory world.</p>
-
-				<p> • When one comes to know Christ, it seems as if everything changes. For that reason, there ought to be a significant difference in believers. II Cor 5:17</p>
-
-				<p> • Do everything in such a way that people are drawn to the wonderful flavor of Jesus.</p>
-			<li> 3. Salt promotes thirst:</li>
-
-				<p> • We are to be the salt of the earth to make people want to drink deeply of the living water that is Jesus. &quot;If anyone ____ let him come to Me and drink. He who believes in Me, as the ____ has said, out of his ____ will flow rivers of living water&quot; John 7:37-38</p>
-
-				<p> • &quot;Preach the Gospel at all times, and if necessary use words.&quot; — Francis of Assisi</p></li>
-			<li>4. Salt permeates food:</li>
-
-				<p> • One person may be as small as a grain of salt compared to the world, but even one grain of salt is noticeable when tasted.</p>
-
-				<p> • William Wilberforce is an example. Almost single-handedly he brought an end to England&#39;s participation in the slave trade. He was small, sickly, and did not appear to be a man of any great impact. God however used him to convict the English parliament that trafficking in human slaves was wrong. England outlawed the practice.</p>
-		</ol>
-</ol>
-
-<p>Next week We are the light of the world </p>
-
-<p>HAPPY STUDYING</p>
-
-
-<hr>	
-</div>
-
-<div>
-<h2>June 24, 2020</h2>
-
-<p>Subject: Sanford Silver, Sr.</p>
-
-<p>Sorry to send this message but Mr. Sanford Silver, Sr. pass this morning.</p>
-
-<p>Will update when the information comes in regarding the service and other info.</p>
-
-<p>Sanford Silver, Jr. address is 105 Sonoma Court, Stephens City, VA 22655</p>
-
-<p>Thanks</p>
-
-<hr>
-
-</div>
-
-<div>
-<h2>June 18, 2020</h2>
-
-<p>Subject: Prayer needs</p>
-
-<p>Prayers for two of the sweetest Ladies in our church family</p>
-
-<p>Leslie Beach is having Cataract surgery tomorrow June 19th at the Winchester Eye Surgery Center. Request is to pray all will go well and God will watch over her.</p>
-
-<p>Peggy Brewer is having surgery July 1. Pray also that all will go well and God will watch over her.</p>
-
-<p>Also please continue to pray for Mr. Sanford Silver. He is still in the hospital (ICU) he can mumble a little and is able to cough. They have taken the tube out of his throat and placed the food tube in his side. All vitals are holding steady with medication. This is small improvements but they are improvements! Your prayers are so needed for him. He loves it up here at SVBC and we miss seeing him.</p>
-
-<p>Prayer that God will give each of these special ones peace and that they will feel comfort from His arms wrapped around them.</p>
-
-<p>Blessings.</p>
-
-
-<hr>
-</div>
-
 
 
 <?php include('includes/footer.php') ?>
