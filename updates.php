34ba49ab615f96d50865775f9aa58da0e319c2df
David M Lewis <me@dlewis.info>
2020-07-21 18:52:44 -0400

    Updated 20200721

--- updates.php
+++ updates.php
@@ -18,9 +18,28 @@
 </div>
 
 <div>
+<h2>July 21, 2020</h2>
+
+<p>Subject - Updates</p>
+
+<p>Margaret Sandretzkys surgery went well. She was able to come home last evening. Prayers for a complete recovery. They both appreciates all your prayers. 132 Stonebrook Rd. Winchester,Va 22602</p>
+
+<p>Peggy Brewer is doing well. She enjoys all the cards and phone calls. 729 Round Hill Rd. Winchester, Va 22602</p>
+
+<p>Pastor Farel is not doing well. The family appreciates your prayers. 366 Gough Rd. Winchester, Va 22602</p>
+
+<p>Dallas Hartley is in WMC with a serious infection. He has not been well for some time. Carolyn is also not doing well. Prayers are appreciated. 4374 Martinsburg Pk. Clearbrook, Va 22624 ( Connie&#39;s sister &amp; brother-in-law)</p>
+
+<p>Prayers for all please and send those cards, they do make a difference.</p>
+
+
+<hr>
+</div>
+
+<div>
 <h2>July 22, 2020</h2>
 <p>Subject - Pastor's Devotional</p>
-<iframe src="private/updates/devotion_20200716.pdf" width="98%" height="700"></iframe>
+<iframe src="private/updates/devotion_20200716.pdf" width="96%" height="700"></iframe>
 <hr>
 </div>
 
@@ -141,84 +160,6 @@
 <hr>
 </div>
 
-<div>
-<h2>July 11, 2020</h2>
-
-<p>Subject - prayer needs and security meeting</p>
-
-<p>Be in prayer for Garrett and Nicole as they head back to WV. Garrett’s grandfather had a massive heart attack this past week and the family has been called together to make the decision as to whether or not to disconnect him from the life sustaining equipment. Pray for God’s peace and comfort for the Sheppard family at this time.</p>
-
-<p>A brief gathering will be held after the service tomorrow for those who have taken the security training in regards to forming a security team at SVBC. Those who attended along with those who would be interested in taking part are encouraged to attend as we decide when to hold a formal meeting ASAP to go over procedures and scheduling.</p>
-
-<p>Please pray for Dallas Hartley and Carolyn as Dallas has been having some serious issues and had to be taken to the hospital today after the second fall this morning. He has been falling a lot. He also has stage 4 kidney failure. So pray that they will be able to get some rest and that this can be resolved in some way. (Connie Sales sister &amp; brother-in-law)</p>
-
-
-<hr>
-</div>
-
-<div>
-<h2>July 09, 2020</h2>
-
-<p>Subject: Pastor’s Devotional</p>
-
-<p>Worn-Out Knees — Living a yielded life </p>
-
-<p>John 14:12-14</p>
-
-<p>Some might think it arrogant when they hear a Christian talk about doing greater works than Jesus. But it was Jesus himself who said His followers would do such works — not greater physical miracles, but spiritual miracles that would have their basis in a life of prayer. The key to Jesus&#39; power on earth was His relationship to the Father in heaven.</p>
-
-<p>I. The EXPECTATION of greater works vs 12a</p>
-
-<p>• The expectation of &quot;greater works&quot; has caused no small amount of confusion and controversy in the body of Christ.</p>
-
-<p>• Jesus said what we would do would result in an even greater impact than what He had done.</p>
-
-<p>II. The EXPLANATION of greater works vs 12b</p>
-
-<p>• Jesus tied the &quot;greater works&quot; to His return to heaven</p>
-
-<p>• Jesus returning to the Father and the Spirit&#39;s coming from the Father—the followers of Jesus would be able to ask of Jesus His help in carrying out His will, continuing to do His works — even greater!</p>
-
-<p>III. The EXPRESSION of greater works vs 12b</p>
-
-<p>• Approximately forty days after Jesus told the disciples about the &quot;greater works&quot;, He returned to heaven then ten days later, on Pentecost, the Holy Spirit was poured out on the disciples resulting in 3,000 coming to Christ! Never in Jesus&#39; ministry did 3,000 people believe in Him in a single day. The &quot;greater works&quot; had begun!</p>
-
-<p>• The REACH of our ministry is greater</p>
-
-<p>• The RESULT of our ministry is greater</p>
-
-<p>IV. The EXECUTION of greater works vs 13-14</p>
-
-<p>• The way we execute these &quot;greater works&quot; is through prayer: &quot;Whatever you ask in My name, that I will do, that the Father may be glorified in the Son. If you ask anything in My name, I will do it&quot;</p>
-
-<p>• It is better to have Jesus within us through the Spirit than beside us.</p>
-
-<p>Ques: vs 10, How could obeying the Father&#39;s will and doing His work lead to conflict in the</p>
-
-<p>world? Which is more important — avoiding conflict or doing God&#39;s will?</p>
-
-<p>Ques: What good news about the Spirit&#39;s ministry is found in John 3:34?</p>
-
-<p>Happy Studying</p>
-
-
-
-<hr>
-</div>
-
-<div>
-<h2>July 06, 2020</h2>
-
-<p>Subject: Peggy Brewer update</p>
-
-<p>Peggy called yesterday and said she is doing just fine. She has no pain which she refused the pain pills from the doctor because she said I don&#39;t have any pain! </p>
-
-<p>She thanks her SVBC family for all the prayers, lovely cards and phone calls. She said it was your prayers and God that brought her through her surgery and now her recovery. She said I will be back to church in a couple of weeks that she would come this Sunday but her family said that she needs to stay away a couple of weeks because of her immune system with this virus. She so loves getting cards. Even if you have already sent her one maybe send another one telling her you are so happy and thankful for her.</p>
-
-<p>Be Blessed</p>
-
-<hr>
-</div>
 
 
 <?php include('includes/footer.php') ?>
