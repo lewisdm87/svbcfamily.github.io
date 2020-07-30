82c6b5c75d29f0c4674df53385df2004978a8585
David M Lewis <me@dlewis.info>
2020-07-30 19:13:18 -0400

    Updated 20200730

--- updates.php
+++ updates.php
@@ -18,6 +18,16 @@
 </div>
 
 <div>
+<h2>July 30, 2020</h2>
+
+<p>Subject - Pastor's Devotional</p>
+
+<iframe src="private/updates/devotional_20200730.pdf" width="96%" height="700"></iframe>
+<hr>
+
+</div>
+
+<div>
 <h2>July 29, 2020</h2>
 
 <p>Subject - Love Offering</p>
@@ -87,131 +97,6 @@
 
 <hr>
 </div>
-
-<div>
-<h2>July 22, 2020</h2>
-<p>Subject - Pastor's Devotional</p>
-<iframe src="private/updates/devotion_20200716.pdf" width="96%" height="700"></iframe>
-<hr>
-</div>
-
-<div>
-<h2>July 13, 2020</h2>
-
-<p>3 of 3 Emails today. &nbsp;Please pardon the number but trying to keep them short. &nbsp;One each on curriculum, training dates, and Zoom call.</p>
-
-<p>Things are a little crazy these days and churches are making plans to reopen children&#39;s ministry on Sunday mornings as well as Awana. &nbsp;We have had several conversations with local churches plus we&#39;re gathering information from other Awana missionaries. &nbsp;A lot of great ideas customized to their church community and building resources. &nbsp;In an effort to share ideas we&#39;re scheduling two Zoom calls next week to have that discussion on a broader basis. &nbsp;Both calls will be the same format so you are welcome to attend one or both. &nbsp;Here&#39;s some of the areas for discussion:</p>
-
-<p>Registration and arrival, small and large groups, game and activity times, and club dismissal. &nbsp;Some clubs are doing all onsite, others a hybrid, and we have some starting off as all online. &nbsp;How do you do safe social distancing and proper sanitary practices while still accomplishing discipleship? &nbsp;Hope that you can join us for the discussion. &nbsp;We have also included the latest Awana Around the World with the 125 countries using Awana to reach more than 4.8 million children. &nbsp;Your church is one of the 63,408 doing Awana to reach more kids.</p>
-<br>
-<p>Click on the link below to attend the meeting of your choice. &nbsp;(the links are unique)</p>
-
-<p>Zoom - Tuesday, July 14 at 7:00 pm</p>
-
-<p>Zoom - Wednesday, July 15 at 7:00 pm</p>
-
-<p>Awana Around the World - Summer 2020</p>
-
-<p>Blessings</p>
-<br>
-<p>Neal and Karole Dillard, Virginia Missionary and U.S. Field Director Awana&nbsp;</p>
-
-<p>E:&nbsp;neald@awana.org&nbsp; &nbsp; P: 703 314-9351 &nbsp;W:&nbsp;www.awanavirginia.org</p>
-
-<p>More about us:&nbsp;&nbsp;Neal &amp; Karole</p>
-
-<p>Copyright © 2020 Awana, All rights reserved.</p>
-<br>
-<p>You are receiving this email because you are a leader in an Awana program.</p>
-<br>
-<p>Our mailing address is:</p>
-
-<p>Awana</p>
-
-<p>55 Transom Ct.</p>
-
-<p>Mt. Crawford, Va 22841</p>
-<br>
-<p>Add us to your address book</p>
-
-
-<hr>
-</div>
-
-<div>
-<h2>July 13, 2020</h2>
-
-<p><a class="nav" href="https://mailchi.mp/5709643f86bf/awana-ministry-conferences-and-basics-training?e=72ddfea883" title="browser view">View this email in your browser</a></p>
-
-<p>2 of 3 Emails today. &nbsp;Please pardon the number but trying to keep them short. &nbsp;One each on curriculum, training dates, and Zoom call.</p>
-
-<p>This year in the interest of safety and reaching more leaders, Awana is offering 11 Awana Ministry Conferences and 11 Awana Basics all online. &nbsp;The Awana Ministry Conferences will be held regionally and include general sessions plus 30+ workshops each. &nbsp;The conferences will be on Saturday mornings and start at 8:30 and run until noon. &nbsp;</p>
-<br>
-<p>The Awana Basics are 90-minute training with about 2 hours of recommended prep work to make the best use of the time. &nbsp; 18 online modules are available to dive deeper after the training. &nbsp;There will also be materials available for either leaders or churches to do their own training.</p>
-<br>
-<p>Both of these opportunities to be better equipped are FREE! &nbsp;Registration information will soon be available but&nbsp;wanted to give you some dates to consider in the Eastern time zone. &nbsp;Any leader is welcome to go to any conference, even in another time zone. &nbsp;Churches can decide whether their leaders will just meet from home to attend or whether circumstances permitted they will gather together to attend. &nbsp;Some churches who do additional church specific training could do this in the afternoon following the Awana Ministry Conference. &nbsp;You may want to extend this invitation to potential leaders who may want more information before committing. &nbsp;</p>
-<br>
-<p>Thursday, July 30, 2020 at 6:30 - 8:00 PM - Awana Basics&nbsp;Online&nbsp;</p>
-
-<p>Monday, August 10, 2020 at 7:00 - 8:30 PM - Awana Basics Online</p>
-
-<p>Wednesday, August 12, 2020 at 7:00 - 8:30 PM - Awana Basics Online</p>
-
-<p>Saturday, August 15, 2020 at 7:00 - 8:30 PM - Awana Basics Online</p>
-
-<p>Saturday, August 15, 2020 at 8:30 - noon - Awana Ministry Conference Southeast&nbsp;</p>
-
-<p>Saturday, September 12, 2020 at&nbsp;8:30 - noon - Awana Ministry Conference Mid-Atlantic</p>
-
-<p>Saturday, September 19, 2020 at&nbsp;8:30 - noon - Awana Ministry Conference North Central</p>
-
-<p>Saturday, October 17, 2020 at&nbsp;8:30 - noon - Awana Ministry Conference Northeast</p>
-
-<p>Blessings</p>
-<br>
-<p>Neal and Karole Dillard, Virginia Missionary and U.S. Field Director Awana&nbsp;</p>
-
-<p>E:&nbsp;neald@awana.org&nbsp; &nbsp; P: 703 314-9351 &nbsp;W:&nbsp;www.awanavirginia.org</p>
-
-<p>More about us:&nbsp;&nbsp;Neal &amp; Karole</p>
-
-<p>Copyright © 2020 Awana, All rights reserved. </p>
-
-
-<hr>
-</div>
-
-<div>
-<h2>July 13, 2020</h2>
-
-<p>Subject - prayer needs</p>
-
-<p>Garrett&#39;s grandfather passed yesterday evening. Please be in prayer for the family. No details at this time.</p>
-
-<blockquote>
-<p>Garrett &amp; Nicole Sheppard</p>
-
-<p>81 Owl Ave. </p>
-
-<p>Martinsburg, WV 25403</p>
-</blockquote>
-
-<p>Ginny DeWeese and Houston both tested positive for the Virus. </p>
-
-<p>Neither has any symptoms at this time. Please be in prayer for them and for God&#39;s protection around them. </p>
-
-<blockquote>
-<p>Ginny DeWeese</p>
-
-<p>P.O. Box 2255</p>
-
-<p>Winchester, VA. 22604</p>
-</blockquote>
-
-
-<hr>
-</div>
-
 
 
 <?php include('includes/footer.php') ?>
