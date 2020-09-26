19c6232ff2e6e9084c40beefea3b748587a24102
David M Lewis <me@dlewis.info>
2020-09-26 19:07:29 -0400

    Updated slides for 20200926

--- private/slides.php
+++ private/slides.php
@@ -126,7 +126,7 @@ var x = setInterval(function() {
 <!--update weekly slides    w=950, h=520-->
 <div class="slides">
 
-<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSi_wHL4dBd8SCM-2J1CNgGIT0qL2t5mZ_7aiQBH46iRWFqCvOm1_pk9JLapfKSLzHU_ByqkSukPS2D/embed?start=false&loop=true&delayms=10000" frameborder="0" width="950" height="520" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSjh2a8NTKAVD9pXY2laPgS6sZRmh6kjfy3-ZtO5D0ohnB3loM6SCirBgp8HWmSMb3zLvTbpQunB0Jz/embed?start=false&loop=true&delayms=10000" frameborder="0" width="950" height="520" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
 
 </div>
 
